3851196ad4dcd97b1eb62281fddc72547f25fca8
MeesPos <meespostma_@hotmail.com>
2020-06-15 22:01:22 +0200

    Eerste versie Header

--- private/views/_navigation.php
+++ private/views/_navigation.php
@@ -1,5 +1,11 @@
-<ul>
-    <li>
-        <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'home' ) ): ?> class="active"<?php endif ?>>Home</a>
-    </li>
-</ul>
+<section id="nav">
+    <div id="navbar">
+        <div class="nav">
+            <label for="toggle" id="label">&#9776;</label>
+            <input type="checkbox" id="toggle">
+            <div class="menu sticky">
+                <a class="nav-items" href="<?php echo url('home') ?>" <?php if (current_route_is('home')) : ?> id="active" <?php endif ?>>Home</a>
+            </div>
+        </div>
+    </div>
+</section>
