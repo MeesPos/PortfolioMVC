5ab751fda6445668164cbdf63daf3ae1cd057daf
MeesPos <meespostma_@hotmail.com>
2020-06-16 17:24:11 +0200

    Header af

--- private/views/_navigation.php
+++ private/views/_navigation.php
@@ -5,6 +5,12 @@
             <input type="checkbox" id="toggle">
             <div class="menu sticky">
                 <a class="nav-items" href="<?php echo url('home') ?>" <?php if (current_route_is('home')) : ?> id="active" <?php endif ?>>Home</a>
+                <a class="nav-items">Over Mij</a>
+                <a class="nav-items">Projecten</a>
+                <a class="nav-items">Tutorials</a>
+                <button class="contact">
+                    <a class="nav-items">Contact</a>
+                </button>
             </div>
         </div>
     </div>
--- private/views/homepage.php
+++ private/views/homepage.php
@@ -18,20 +18,30 @@
 				<?php echo $this->fetch('_navigation') ?>
 			<?php endif ?>
 		</nav>
-
-		<section id="header">
-			<div class="name">
-				<h2>SOFTWARE</h2>
-				<h1>MEES POSTMA</h1>
-				<h2>DEVELOPER</h2>
-			</div>
-
-			<div class="rocket">
-				<img src="<?php echo site_url('/img/rocket.png')?>" alt="Rocket Full" class="rocket-full">
-				<img src="<?php echo site_url('/img/rocket.png')?>" alt="Rocket Full" class="rocket-opp">
-			</div>
-		</section>
 	</header>
+
+
+
+	<section id="header">
+		<div class="intro-right-gradient"></div>
+		<div class="information">
+			<div class="nameme">
+				<h1 class="name">MEES POSTMA</h1>
+				<h2 class="swdev">SOFTWARE DEVELOPER</h2>
+				<div class="border-bot"></div>
+				<p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					<br>Quibusdam ipsa mollitia totam, dicta accusantium<br> sit doloremque ipsam exercitationem
+					blanditiis!</p>
+				<button class="moreinfo">Meer Info!</button>
+			</div>
+
+
+			<div class="animation">
+				<img class="webani" src="<?php echo site_url('/img/heroheader.png') ?>" alt="Web developer">
+			</div>
+		</div>
+	</section>
+
 </body>
 
 </html>
