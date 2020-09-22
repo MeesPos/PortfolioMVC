e02fce93be671bdbebe89930d8f7d657e58b832f
MeesPos <meespostma_@hotmail.com>
2020-09-22 22:20:00 +0200

    Center bug bij over dit project

--- private/views/projectDetail.php
+++ private/views/projectDetail.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css">
 </head>
 
-<body>
+<body class="projectDetail">
     <?php if ($this->section('navigation')) : ?>
         <?php echo $this->section('navigation') ?>
     <?php else : ?>
@@ -39,22 +39,23 @@
                     <a href="https://cdn.thecrazytourist.com/wp-content/uploads/2018/02/Boat-Trips.jpg" data-gallery="thumb">
                         <img src="https://cdn.thecrazytourist.com/wp-content/uploads/2018/02/Boat-Trips.jpg">
                     </a>
+                    <a href="https://cdn.thecrazytourist.com/wp-content/uploads/2018/02/Boat-Trips.jpg" data-gallery="thumb">
+                        <img src="https://cdn.thecrazytourist.com/wp-content/uploads/2018/02/Boat-Trips.jpg">
+                    </a>
                 </div>
             </div>
         </div>
 
         <div class="projectInfoText">
             <?php foreach ($projectDetails as $row) : ?>
-                <h2 class="overProject">Over dit Project</h2>
+                <h2 class="overProject">Over dit <span style="font-weight: bold;">Project</span></h2>
+                <h3 class="soortProject">
+                    <p><?php echo $row['categorie'] ?></p>
+                </h3>
                 <p class="projectSamenvatting"><?php echo $row['samenvatting'] ?></p>
                 <div class="specificatiesProj">
-                    <div class="soortProject">
-                        <h3>Soort Project</h3>
-                        <p><?php echo $row['categorie'] ?></p>
-                    </div>
-
                     <div class="gemaaktMet">
-                        <h3>Gemaakt met</h3>
+                        <h3>Gemaakt met: </h3>
                         <?php foreach ($madeWith as $made) : ?>
                             <div class="madeItem">
                                 <p><?php echo $made['naam']; ?>,</p>
@@ -63,24 +64,30 @@
                     </div>
 
                     <div class="taal">
-                        <h3>Taal</h3>
-                        <?php echo $row['taal']; ?>
+                        <h3>Taal: </h3>
+                        <p><?php echo $row['taal']; ?></p>
                     </div>
                 </div>
             <?php endforeach; ?>
+
+            <div class="gemaaktButtons">
+                <a href="#">
+                    <button>Live versie</button>
+                </a>
+
+                <a href="#">
+                    <button><i class="fab fa-github"></i> Github</button>
+                </a>
+            </div>
         </div>
     </section>
 
-
-    <!-- Beschrijving van Project -->
-    <!-- Link naar project -->
-
-    <!-- Code Informatie -->
-
-    <!-- Overige images -->
-
-    <!-- Contact verwijzing -->
-
+    <section id="meerinfo">
+        <div class="infoContent">
+            <h2>Meer over <span style="font-weight: bold;">dit Project</span></h2>
+            <p><?php echo $row['content']; ?></p>
+        </div>
+    </section>
     <?php if ($this->section('footer')) : ?>
         <?php echo $this->section('footer') ?>
     <?php else : ?>
@@ -92,6 +99,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-zoom/1.7.21/jquery.zoom.js"></script>
     <script src="https://kit.fontawesome.com/a82e000026.js"></script>
     <script src="<?php echo site_url('/js/gallery.js') ?>"></script>
-</body>
+</body=>
 
 </html>
